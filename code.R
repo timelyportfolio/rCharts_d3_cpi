5c6f93e70d8d9f1244ee47a7ca9bf25f67977bc1
timelyportfolio <kent.russell@timelyportfolio.com>
2014-04-03 16:18:00 -0500

    approaching an ugly working first draft

--- code.R
+++ code.R
@@ -1,6 +1,8 @@
 #here are the two data source, but really don't need to do this in R
 #could easily supply the locations as data instead
 #and use d3.tsv
+#but might generate the filters in R just to show combo
+
 
 #put items in data frame
 items.df = read.delim(
@@ -10,6 +12,7 @@ items.df = read.delim(
   , stringsAsFactors=FALSE
   , strip.white=TRUE
 )[,-2,drop=F]
+items.df[,1] <- gsub(x=items.df[,1],pattern='\\"',replacement="")
 
 #load the average price file which is large
 prices.df = read.delim(
@@ -25,11 +28,20 @@ options(viewer=NULL)
 
 cpi1 <- rCharts$new()
 cpi1$setLib(".")
-cpi1$addAssets(
-  jshead = "http://cdnjs.cloudflare.com/ajax/libs/angular.js/1.2.1/angular.min.js"
-)
+cpi1$lib <- "d3_cpi"
+#cpi1$addAssets(
+  #possible bug in rCharts here since getting added to both css and js when css defined
+  #in config.yml
+#  css = NULL,
+#  jshead = "http://cdnjs.cloudflare.com/ajax/libs/angular.js/1.2.1/angular.min.js"
+#)
 cpi1$setTemplate(
   chartDiv = '<div></div>',
   afterScript = '<script></script>'
 )
-cpi1
+cpi1$set(
+  bodyattrs = "ng-app"
+)
+cpi1$params$filters = data.frame(code=rownames(items.df),items.df,stringsAsFactors = F)
+cpi1$save("index.html",cdn=T)
+
